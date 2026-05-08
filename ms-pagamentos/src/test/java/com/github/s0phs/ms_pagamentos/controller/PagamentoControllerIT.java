e0adce57175dae8c3737d87c09265a849d8b734d
Laboratório FIAP <labsfiap@fiap.com.br>
2026-05-08 10:29:27 -0300

    feat: update do import.sql, add findAll PagamentoControllerIT

---
+++ ms-pagamentos/src/test/java/com/github/s0phs/ms_pagamentos/controller/PagamentoControllerIT.java
@@ -0,0 +1,61 @@
+package com.github.s0phs.ms_pagamentos.controller;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.s0phs.ms_pagamentos.entities.Pagamento;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@Transactional
+public class PagamentoControllerIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private Pagamento pagamento;
+    private Long existingId;
+    private Long nonExistingId;
+
+    @BeforeEach
+    void setUp(){
+        existingId = 1L;
+        nonExistingId = Long.MAX_VALUE;
+    }
+
+    @Test
+    void findPagamentosShouldReturn200AndJsonArray() throws Exception {
+
+        mockMvc.perform(get("/pagamentos")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$[0].id").isNumber())//array começa com 0, id começa com 1
+                .andExpect(jsonPath("$[*].id").isArray())
+                .andExpect(jsonPath("$[1].valor").value(3599.0));
+    }
+
+    
+
+}
